171b235dfbc12e03a068ae4f93a735ef1e3ec5cb
“cjhgit” <1418503647@qq.com>
2022-05-31 23:57:38 +0800

    commit initial

---
+++ README.md
@@ -0,0 +1,10 @@
+# react-template
+
+React 应用模板。
+
+
+## 技术栈
+
+* React 17
+* Vite
+* LESS module
---
+++ src/app.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import classes from './app.module.less'
+
+export default function App() {
+
+    return (
+        <div>
+            <div className={classes.hello}>Hello World!</div>
+        </div>
+    )
+}
